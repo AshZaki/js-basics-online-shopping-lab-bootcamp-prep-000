1f32384ec7bda106d4dc8fe3de414a3007f2888e
Ashley  Zaki <ahnahn.un@gmail.com>
2018-08-12 22:57:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,10 +68,13 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
-  if (cardNumber === 0){
-    return `Sorry, we don\'t have a credit card on file for you.`
-  }
+  for (let i = 0; i < cart.length ; i++){
+    if (cart.cardNumber === 0)
+    return `Sorry, we don't have a credit card on file for you.`
+    }
+  
 }
+
 
 
 
