32fe5b6dd14b5fd5e966e0ab37ae1104056c869a
Ashley  Zaki <ahnahn.un@gmail.com>
2018-08-12 22:47:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,12 +56,40 @@ function total() {
 
 
 function removeFromCart(item) {
-  var cItem;
-  cart.slice(item);
-  return cart;
-  
+  for (let i = 0; i< cart.length; i++){
+    if (cart[i].itemName == item){
+      cart.splice(i,1)
+      return cart;
+    }
+  }
+  return "That item is not in your cart.";
 }
+  
+
 
 function placeOrder(cardNumber) {
   // write your code here
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
